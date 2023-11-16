87ef546bdb6c3ca831b345229bfeacb0ca8b2ff3
josedsferreira <94454444+josedsferreira@users.noreply.github.com>
2023-11-16 19:59:23 +0000

    Commit inicial

---
+++ app/src/main/java/com/example/qrapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.qrapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val scanButton = findViewById<Button>(R.id.scanButton)
+
+        scanButton.setOnClickListener {
+            val intent = Intent(this, ScannerActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
