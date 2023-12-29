d1a8dacb7e26ce53a04bad0fefb82e029d9836b7
miguelcarvalho <miguelvdscarvalho@gmail.com>
2023-12-29 17:44:29 +0000

    menus finalizados

--- app/src/main/java/com/example/qrapp/ScannerActivity.kt
+++ app/src/main/java/com/example/qrapp/ScannerActivity.kt
@@ -3,6 +3,7 @@ package com.example.qrapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.Firebase
@@ -67,10 +68,16 @@ class ScannerActivity : AppCompatActivity() {
                                     // A operação falhou
                                     println("Erro ao atualizar o status 'is_in': ${e.message}")
                                 }
+
                         }
                     }
                 }
                 updateIsInStatus(scannedData,true)
+                val lqrbutton = findViewById<Button>(R.id.lerqr)
+                lqrbutton.setOnClickListener {
+                    val intent = Intent(this, ScannerActivity::class.java)
+                    startActivity(intent)
+                }
             } else {
                 // Handle no result
             }
